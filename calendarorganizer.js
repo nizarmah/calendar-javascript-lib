f0d090916cc6abd0a8033c9c89e5d43a005e08b3
Nizar <nizarmah@hotmail.com>
2018-01-14 05:23:20 -0800

    Current Day Indicator + IE 10+ Support

--- calendarorganizer.js
+++ calendarorganizer.js
@@ -29,7 +29,9 @@ function Calendar(id, size, labelSettings, colors, options) {
     for (var i = 0; i < 7; i++) {
         this.labels.push(this.label[i].substring(0, labelSettings[1] > 3 ? 3 : labelSettings[1]));
     }
+
     this.date = new Date();
+    this.today = new Date();
 
     this.draw();
     this.update();
@@ -50,8 +52,7 @@ Calendar.prototype = {
             if (date.getDate() > previousLastDay) {
                 this.changeDateTo(previousLastDay);
             }
-            if (date.getMonth() > 0) date.setMonth(date.getMonth() - 1);
-            else {
+            if (date.getMonth() > 0) date.setMonth(date.getMonth() - 1);else {
                 date.setMonth(11);
                 date.setFullYear(date.getFullYear() - 1);
             }
@@ -68,8 +69,7 @@ Calendar.prototype = {
             if (date.getDate() > soonLastDay) {
                 this.changeDateTo(soonLastDay);
             }
-            if (date.getMonth() != 11) date.setMonth(date.getMonth() + 1);
-            else {
+            if (date.getMonth() != 11) date.setMonth(date.getMonth() + 1);else {
                 date.setMonth(0);
                 date.setFullYear(date.getFullYear() + 1);
             }
@@ -79,13 +79,12 @@ Calendar.prototype = {
     },
     changeDateTo: function changeDateTo(theDay, theBlock) {
         if (theBlock >= 31 && theDay <= 11 || theBlock <= 6 && theDay >= 8) {
-            if (theBlock >= 31 && theDay <= 11) this.next('month');
-            else if (theBlock <= 6 && theDay >= 8) this.back('month');
+            if (theBlock >= 31 && theDay <= 11) this.next('month');else if (theBlock <= 6 && theDay >= 8) this.back('month');
 
             this.date.setDate(theDay);
 
             var calendarInstance = this;
-            setTimeout(function() {
+            setTimeout(function () {
                 calendarInstance.update();
             }, 1);
 
@@ -94,16 +93,17 @@ Calendar.prototype = {
     }
 };
 
-Calendar.prototype.draw = function() {
+Calendar.prototype.draw = function () {
     var backSvg = '<svg style="width: 24px; height: 24px;" viewBox="0 0 24 24"><path fill="' + this.colors[3] + '" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"></path></svg>';
     var nextSvg = '<svg style="width: 24px; height: 24px;" viewBox="0 0 24 24"><path fill="' + this.colors[3] + '" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path></svg>';
 
     var theCalendar = document.createElement("DIV");
     theCalendar.className = "calendar " + this.size;
 
-    document.getElementById(this.id).appendChild(theCalendar);
+    document.getElementById(this.id).appendChild(theCalendar.cloneNode(true));
 
-    var theContainers = [], theNames = ['year', 'month', 'labels', 'days'];
+    var theContainers = [],
+        theNames = ['year', 'month', 'labels', 'days'];
     for (var i = 0; i < theNames.length; i++) {
         theContainers[i] = document.createElement("DIV");
         theContainers[i].className = theNames[i];
@@ -117,16 +117,16 @@ Calendar.prototype.draw = function() {
                     var backSlider = document.createElement("DIV");
                     backSlider.id = this.id + "-year-back";
                     backSlider.insertAdjacentHTML('beforeend', backSvg);
-                    theContainers[i].appendChild(backSlider);
+                    theContainers[i].appendChild(backSlider.cloneNode(true));
 
                     var theText = document.createElement("SPAN");
                     theText.id = this.id + "-" + theNames[i];
-                    theContainers[i].appendChild(theText);
+                    theContainers[i].appendChild(theText.cloneNode(true));
 
                     var nextSlider = document.createElement("DIV");
                     nextSlider.id = this.id + "-year-next";
                     nextSlider.insertAdjacentHTML('beforeend', nextSvg);
-                    theContainers[i].appendChild(nextSlider);
+                    theContainers[i].appendChild(nextSlider.cloneNode(true));
                 }
             } else {
                 theContainers[i].style.backgroundColor = this.colors[0];
@@ -135,16 +135,16 @@ Calendar.prototype.draw = function() {
                 var backSlider = document.createElement("DIV");
                 backSlider.id = this.id + "-month-back";
                 backSlider.insertAdjacentHTML('beforeend', backSvg);
-                theContainers[i].appendChild(backSlider);
+                theContainers[i].appendChild(backSlider.cloneNode(true));
 
                 var theText = document.createElement("SPAN");
                 theText.id = this.id + "-" + theNames[i];
-                theContainers[i].appendChild(theText);
+                theContainers[i].appendChild(theText.cloneNode(true));
 
                 var nextSlider = document.createElement("DIV");
                 nextSlider.id = this.id + "-month-next";
                 nextSlider.insertAdjacentHTML('beforeend', nextSvg);
-                theContainers[i].appendChild(nextSlider);
+                theContainers[i].appendChild(nextSlider.cloneNode(true));
             }
         }
     }
@@ -152,12 +152,14 @@ Calendar.prototype.draw = function() {
     for (var i = 0; i < this.labels.length; i++) {
         var theLabel = document.createElement("SPAN");
         theLabel.id = this.id + "-label-" + (i + 1);
-        theLabel.appendChild(document.createTextNode(this.labels[i]));
+        theLabel.appendChild(document.createTextNode(this.labels[i]).cloneNode(true));
 
-        theContainers[2].appendChild(theLabel);
+        theContainers[2].appendChild(theLabel.cloneNode(true));
     }
 
-    var theRows = [], theDays = [], theRadios = [];
+    var theRows = [],
+        theDays = [],
+        theRadios = [];
     for (var i = 0; i < 6; i++) {
         theRows[i] = document.createElement("DIV");
         theRows[i].className = "row";
@@ -178,10 +180,10 @@ Calendar.prototype.draw = function() {
         var theText = document.createElement("SPAN");
         theText.id = this.id + "-day-num-" + (i + 1);
 
-        theDays[i].appendChild(theText);
+        theDays[i].appendChild(theText.cloneNode(true));
 
-        theRows[j].appendChild(theRadios[i]);
-        theRows[j].appendChild(theDays[i]);
+        theRows[j].appendChild(theRadios[i].cloneNode(true));
+        theRows[j].appendChild(theDays[i].cloneNode(true));
 
         if ((i + 1) % 7 == 0) {
             j++;
@@ -189,18 +191,18 @@ Calendar.prototype.draw = function() {
     }
 
     for (var i = 0; i < 6; i++) {
-        theContainers[3].appendChild(theRows[i]);
+        theContainers[3].appendChild(theRows[i].cloneNode(true));
     }
 
     for (var i = 0; i < theContainers.length; i++) {
-        theCalendar.appendChild(theContainers[i]);
+        theCalendar.appendChild(theContainers[i].cloneNode(true));
     }
 
-    document.getElementById(this.id).innerHTML = "<style>.day.active::before { background-color: " + this.colors[1] + "; }</style>";
-    document.getElementById(this.id).appendChild(theCalendar);
+    document.getElementById(this.id).innerHTML = "<style>.day.active::before { background-color: " + this.colors[1] + "; } .day.today > span { border-color: " + this.colors[1] + " !important; }</style>";
+    document.getElementById(this.id).appendChild(theCalendar.cloneNode(true));
 };
 
-Calendar.prototype.update = function() {
+Calendar.prototype.update = function () {
     document.getElementById(this.id + '-year').innerHTML = this.date.getFullYear();
     document.getElementById(this.id + '-month').innerHTML = this.months[this.date.getMonth()];
 
@@ -223,7 +225,11 @@ Calendar.prototype.update = function() {
 
     for (var i = 1; i <= lastDay; i++) {
         document.getElementById(this.id + '-day-num-' + (this.label.indexOf(this.label[firstDay]) + i)).innerHTML = i;
+
         if (i == this.date.getDate()) document.getElementById(this.id + '-day-radio-' + (this.label.indexOf(this.label[firstDay]) + i)).checked = true;
+
+        if (this.date.getMonth() == this.today.getMonth())
+            if (i == this.today.getDate()) document.getElementById(this.id + '-day-' + (this.label.indexOf(this.label[firstDay]) + i)).className += " today";
     }
 
     for (var i = lastDay + 1, j = 1; this.label.indexOf(this.label[firstDay]) + i <= 42; i++, j++) {
@@ -232,8 +238,8 @@ Calendar.prototype.update = function() {
     }
 };
 
-Calendar.prototype.setupBlock = function(blockId, calendarInstance, callback) {
-    document.getElementById(calendarInstance.id + "-day-" + blockId).onclick = function() {
+Calendar.prototype.setupBlock = function (blockId, calendarInstance, callback) {
+    document.getElementById(calendarInstance.id + "-day-" + blockId).onclick = function () {
         if (document.getElementById(calendarInstance.id + "-day-num-" + blockId).innerHTML.length > 0) {
             calendarInstance.changeDateTo(document.getElementById(calendarInstance.id + "-day-num-" + blockId).innerHTML, blockId);
             callback();
@@ -241,11 +247,11 @@ Calendar.prototype.setupBlock = function(blockId, calendarInstance, callback) {
     };
 };
 
-Calendar.prototype.setOnClickListener = function(theCase, backCallback, nextCallback) {
+Calendar.prototype.setOnClickListener = function (theCase, backCallback, nextCallback) {
     var calendarId = this.id;
 
-    backCallback = backCallback || function() {};
-    nextCallback = nextCallback || function() {};
+    backCallback = backCallback || function () {};
+    nextCallback = nextCallback || function () {};
 
     var calendarInstance = this;
 
@@ -256,21 +262,21 @@ Calendar.prototype.setOnClickListener = function(theCase, backCallback, nextCall
             }
             break;
         case "month-slider":
-            document.getElementById(calendarId + "-month-back").onclick = function() {
+            document.getElementById(calendarId + "-month-back").onclick = function () {
                 calendarInstance.back('month');
                 backCallback();
             };
-            document.getElementById(calendarId + "-month-next").onclick = function() {
+            document.getElementById(calendarId + "-month-next").onclick = function () {
                 calendarInstance.next('month');
                 nextCallback();
             };
             break;
         case "year-slider":
-            document.getElementById(calendarId + "-year-back").onclick = function() {
+            document.getElementById(calendarId + "-year-back").onclick = function () {
                 calendarInstance.back('year');
                 backCallback();
             };
-            document.getElementById(calendarId + "-year-next").onclick = function() {
+            document.getElementById(calendarId + "-year-next").onclick = function () {
                 calendarInstance.next('year');
                 nextCallback();
             };
@@ -287,7 +293,7 @@ function Organizer(id, calendar, data) {
     this.draw();
 
     var organizerInstance = this;
-    organizerInstance.onMonthChange(function() {
+    organizerInstance.onMonthChange(function () {
         organizerInstance.indicateEvents();
     });
 
@@ -314,7 +320,7 @@ Organizer.prototype = {
                 this.changeDateTo(lastDay);
 
                 var organizerInstance = this;
-                organizerInstance.onMonthChange(function() {
+                organizerInstance.onMonthChange(function () {
                     organizerInstance.indicateEvents();
                 });
             }
@@ -324,7 +330,7 @@ Organizer.prototype = {
             this.calendar.back(func);
 
             var organizerInstance = this;
-            organizerInstance.onMonthChange(function() {
+            organizerInstance.onMonthChange(function () {
                 organizerInstance.indicateEvents();
             });
         }
@@ -344,7 +350,7 @@ Organizer.prototype = {
                 date.setDate(1);
 
                 var organizerInstance = this;
-                organizerInstance.onMonthChange(function() {
+                organizerInstance.onMonthChange(function () {
                     organizerInstance.indicateEvents();
                 });
             }
@@ -354,7 +360,7 @@ Organizer.prototype = {
             this.calendar.next(func);
 
             var organizerInstance = this;
-            organizerInstance.onMonthChange(function() {
+            organizerInstance.onMonthChange(function () {
                 organizerInstance.indicateEvents();
             });
         }
@@ -363,9 +369,9 @@ Organizer.prototype = {
         var changedMonth = this.calendar.changeDateTo(theDay, theBlock);
 
         var organizerInstance = this;
-        setTimeout(function() {
+        setTimeout(function () {
             if (changedMonth) {
-                organizerInstance.onMonthChange(function() {
+                organizerInstance.onMonthChange(function () {
                     organizerInstance.indicateEvents();
                 });
             } else organizerInstance.update();
@@ -373,7 +379,7 @@ Organizer.prototype = {
     }
 };
 
-Organizer.prototype.draw = function() {
+Organizer.prototype.draw = function () {
     var backSvg = '<svg style="width: 24px; height: 24px;" viewBox="0 0 24 24"><path fill="' + this.calendar.colors[3] + '" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"></path></svg>';
     var nextSvg = '<svg style="width: 24px; height: 24px;" viewBox="0 0 24 24"><path fill="' + this.calendar.colors[3] + '" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path></svg>';
 
@@ -388,16 +394,16 @@ Organizer.prototype.draw = function() {
     var backSlider = document.createElement("DIV");
     backSlider.id = this.id + "-day-back";
     backSlider.insertAdjacentHTML('beforeend', backSvg);
-    theDate.appendChild(backSlider);
+    theDate.appendChild(backSlider.cloneNode(true));
 
     var theText = document.createElement("SPAN");
     theText.id = this.id + "-date";
-    theDate.appendChild(theText);
+    theDate.appendChild(theText.cloneNode(true));
 
     var nextSlider = document.createElement("DIV");
     nextSlider.id = this.id + "-day-next";
     nextSlider.insertAdjacentHTML('beforeend', nextSvg);
-    theDate.appendChild(nextSlider);
+    theDate.appendChild(nextSlider.cloneNode(true));
 
     var theRows = document.createElement("DIV");
     theRows.className = "rows";
@@ -406,22 +412,22 @@ Organizer.prototype.draw = function() {
     theList.className = "list";
     theList.id = this.id + "-list";
 
-    theRows.appendChild(theList);
+    theRows.appendChild(theList.cloneNode(true));
 
-    theOrganizer.appendChild(theDate);
-    theOrganizer.appendChild(theRows);
+    theOrganizer.appendChild(theDate.cloneNode(true));
+    theOrganizer.appendChild(theRows.cloneNode(true));
 
-    document.getElementById(this.id).appendChild(theOrganizer);
+    document.getElementById(this.id).appendChild(theOrganizer.cloneNode(true));
 };
 
-Organizer.prototype.update = function() {
+Organizer.prototype.update = function () {
     document.getElementById(this.id + "-date").innerHTML = this.calendar.months[this.calendar.date.getMonth()] + " " + this.calendar.date.getDate() + ", " + this.calendar.date.getFullYear();
     document.getElementById(this.id + "-list").innerHTML = "";
 
     this.showEvents();
 };
 
-Organizer.prototype.list = function(data) {
+Organizer.prototype.list = function (data) {
     document.getElementById(this.id + "-list").innerHTML = "";
 
     var content = document.createElement("UL");
@@ -451,10 +457,10 @@ Organizer.prototype.list = function(data) {
     document.getElementById(this.id + "-list").innerHTML = content.innerHTML;
 };
 
-Organizer.prototype.setupBlock = function(blockId, organizerInstance, callback) {
+Organizer.prototype.setupBlock = function (blockId, organizerInstance, callback) {
     var calendarInstance = organizerInstance.calendar;
 
-    document.getElementById(calendarInstance.id + "-day-" + blockId).onclick = function() {
+    document.getElementById(calendarInstance.id + "-day-" + blockId).onclick = function () {
         if (document.getElementById(calendarInstance.id + "-day-num-" + blockId).innerHTML.length > 0) {
             organizerInstance.changeDateTo(document.getElementById(calendarInstance.id + "-day-num-" + blockId).innerHTML, blockId);
             callback();
@@ -462,7 +468,7 @@ Organizer.prototype.setupBlock = function(blockId, organizerInstance, callback) 
     };
 };
 
-Organizer.prototype.showEvents = function(data) {
+Organizer.prototype.showEvents = function (data) {
     data = data || this.data;
     var date = this.calendar.date;
 
@@ -471,7 +477,7 @@ Organizer.prototype.showEvents = function(data) {
     } catch (e) {}
 };
 
-Organizer.prototype.indicateEvents = function(data) {
+Organizer.prototype.indicateEvents = function (data) {
     data = data || this.data;
     var date = this.calendar.date;
 
@@ -490,16 +496,16 @@ Organizer.prototype.indicateEvents = function(data) {
     this.update();
 };
 
-Organizer.prototype.onMonthChange = function(callback) {
+Organizer.prototype.onMonthChange = function (callback) {
     callback();
 };
 
-Organizer.prototype.setOnClickListener = function(theCase, backCallback, nextCallback) {
+Organizer.prototype.setOnClickListener = function (theCase, backCallback, nextCallback) {
     var calendarId = this.calendar.id;
     var organizerId = this.id;
 
-    backCallback = backCallback || function() {};
-    nextCallback = nextCallback || function() {};
+    backCallback = backCallback || function () {};
+    nextCallback = nextCallback || function () {};
 
     var organizerInstance = this;
 
@@ -510,31 +516,31 @@ Organizer.prototype.setOnClickListener = function(theCase, backCallback, nextCal
             }
             break;
         case "day-slider":
-            document.getElementById(organizerId + "-day-back").onclick = function() {
+            document.getElementById(organizerId + "-day-back").onclick = function () {
                 organizerInstance.back('day');
                 backCallback();
             };
-            document.getElementById(organizerId + "-day-next").onclick = function() {
+            document.getElementById(organizerId + "-day-next").onclick = function () {
                 organizerInstance.next('day');
                 nextCallback();
             };
             break;
         case "month-slider":
-            document.getElementById(calendarId + "-month-back").onclick = function() {
+            document.getElementById(calendarId + "-month-back").onclick = function () {
                 organizerInstance.back('month');
                 backCallback();
             };
-            document.getElementById(calendarId + "-month-next").onclick = function() {
+            document.getElementById(calendarId + "-month-next").onclick = function () {
                 organizerInstance.next('month');
                 nextCallback();
             };
             break;
         case "year-slider":
-            document.getElementById(calendarId + "-year-back").onclick = function() {
+            document.getElementById(calendarId + "-year-back").onclick = function () {
                 organizerInstance.back('year');
                 backCallback();
             };
-            document.getElementById(calendarId + "-year-next").onclick = function() {
+            document.getElementById(calendarId + "-year-next").onclick = function () {
                 organizerInstance.next('year');
                 nextCallback();
             };
